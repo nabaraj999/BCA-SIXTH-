1c3e7fa636cd46e66e6c5722ffcb8c76c480bd7d
nabaraj999 <nabarajacharya999@gmail.com>
2025-09-08 13:00:38 +0545

    Initial commit - Android Studio project

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -51,4 +51,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+    public void onSignupClick(View view) {
+        Intent intent = new Intent(LoginActivity.this, registerActivity.class);
+        startActivity(intent); // Missing in your code
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/registerActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+}
+
