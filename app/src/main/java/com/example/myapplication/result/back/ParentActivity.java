d271faca22ffa18ec3972831e769911903e81695
nabaraj999 <nabarajacharya999@gmail.com>
2025-10-16 09:00:33 +0545

    Updated code and fixed bugs

--- app/src/main/java/com/example/myapplication/result/back/ParentActivity.java
+++ app/src/main/java/com/example/myapplication/result/back/ParentActivity.java
@@ -1,11 +1,18 @@
 package com.example.myapplication.result.back;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,6 +21,10 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.myapplication.R;
 
 public class ParentActivity extends AppCompatActivity {
+
+
+    private ActivityResultLauncher<Intent> resultAucha;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +36,37 @@ public class ParentActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btnAddUser = findViewById(R.id.button);
+        //Register the launcher to get result
+        resultAucha = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
 
+                    }
+                });
+        /*ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                            String name = result.getData().getStringExtra("User name");
+                            Toast.makeText(ParentActivity.this, "User added:" + name, Toast.LENGTH_SHORT).show();
+                        }
+                        if (result.getResultCode() == RESULT_CANCELED) {
+                            Toast.makeText(ParentActivity.this, "Action Cancled", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });*/
+        //Start child activity when button clicked
+        btnAddUser.setOnClickListener(v -> {
+            Intent intent = new Intent(ParentActivity.this, AddUserActivity.class);
+            resultAucha.launch(intent);
+        });
     }
-
-    public void btnAddUser(View view) {
+    public void onAddClick(View view) {
         new Intent(ParentActivity.this,AddUserActivity.class);
+        resultAucha.launch(intent);
     }
 }
