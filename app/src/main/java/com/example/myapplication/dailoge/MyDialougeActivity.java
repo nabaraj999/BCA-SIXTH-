d84a00addfd5f0bfb6be64dd9d9d422af8f48761
nabaraj999 <nabarajacharya999@gmail.com>
2025-11-03 06:46:55 +0545

    Updated code and fixed bugs

---
+++ app/src/main/java/com/example/myapplication/dailoge/MyDialougeActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.dailoge;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+
+public class MyDialougeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_my_dialouge);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+       Button btnshowDialog=findViewById(R.id.btn_show_dialog);
+       btnshowDialog.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+    }
+}
