346a02400b93d53df17cbb734a64136533a89015
nabaraj999 <nabarajacharya999@gmail.com>
2025-11-06 08:11:04 +0545

    Updated code and fixed bugs

---
+++ app/src/main/java/com/example/myapplication/MyAdapterActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MyAdapterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_my_adapter);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+       Spinner comboBox = findViewById(R.id.spin);
+        ArrayAdapter.
+
+    }
+}
