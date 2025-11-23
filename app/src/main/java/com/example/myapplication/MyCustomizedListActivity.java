2bc3d8632edaef4046fc172fee3a9f9502f832d8
nabaraj999 <nabarajacharya999@gmail.com>
2025-11-23 07:05:51 +0545

    Updated code and fixed bugs

--- app/src/main/java/com/example/myapplication/MyCustomizedListActivity.java
+++ app/src/main/java/com/example/myapplication/MyCustomizedListActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class MyCustomizedListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView list=findViewById(R.id.mero_list);
     }
 }
