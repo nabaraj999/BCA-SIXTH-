ac52297d05f5bd8a97566de8a10d32df57e0cb9a
nabaraj999 <nabarajacharya999@gmail.com>
2025-11-23 07:20:38 +0545

    Updated code and fixed bugs

--- app/src/main/java/com/example/myapplication/MyCustomizedListActivity.java
+++ app/src/main/java/com/example/myapplication/MyCustomizedListActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,5 +26,29 @@ public class MyCustomizedListActivity extends AppCompatActivity {
         });
 
         ListView list=findViewById(R.id.mero_list);
+        meroList.setAdapater();
+    }
+}
+class MeroCoutomAdapter extends BaseAdapter {
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
     }
 }
